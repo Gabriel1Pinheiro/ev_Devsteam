77cb46c359e423bf7951202f9c5e6d5ed2a061e6
Gabriel <gabriel.pinheiro1240@gmail.com>
2023-07-15 15:09:44 -0300

    criação de input e atualizaão de navbar

--- src/Componentes/Navbar/navbar.jsx
+++ src/Componentes/Navbar/navbar.jsx
@@ -1,13 +1,16 @@
 import styles from '@/Componentes/Navbar/navbar.module.css'
 import {BsCart4} from 'react-icons/bs'
 import Logo from '@/Componentes/Logo/logo'
+import Input from '../Forms/Input/input'
 
 export default function Navbar() {
 
     return(
         <nav className={styles.navbar}>
             <Logo/>
-            <input/>
+            <div className={styles.search}>
+            <Input type="text" placeholder="Buscar" fullWidth/>
+            </div>
             <BsCart4 size={40}/>
         </nav>
     )
